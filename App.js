5b7fdcec21c95585e4f2c8c5fda2f8e24d866266
leocassiosilva <leocassiosilva1234@gmail.com>
2021-07-14 01:19:24 -0300

    first commit

--- App.js
+++ App.js
@@ -1,13 +1,23 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { Component } from 'react';
-import { StyleSheet, Text, View, Image} from 'react-native';
+import { StyleSheet, Text, View, Image, TouchableHighlight} from 'react-native';
 
 class App extends Component {
   render(){
     return (
       <View style={styles.container}>
         <Image source={require('./src/biscoito.png')}
-        style={style.img}/>
+        style={styles.img}
+        />
+       
+
+        <Text style={styles.textoFrase}> Alguma Frase Aqui</Text>
+        <TouchableHighlight style={styles.btn}>
+          <View style={styles.btnArea}>
+            <Text style={styles.btnTexto}>Quebrar Biscoito</Text>
+          </View>
+        </TouchableHighlight>
+      
       </View>
     );
   }
@@ -16,13 +26,37 @@ class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
   img:{
     width:250,
     height:250
+  }, 
+  textoFrase:{
+    fontSize:20,
+    color:'#dd7b22',
+    margin:30,
+    fontStyle:'italic',
+    textAlign:'center'
+
+  },
+  btn:{
+    width:230,
+    height:50,
+    borderWidth:2,
+    borderColor:'#dd7b22',
+    borderRadius:25
+  },
+  btnArea:{
+    flex:1,
+    flexDirection:'row',
+    justifyContent:'center', 
+    alignItems:'center'
+  }, 
+  btnTexto:{
+    fontSize:18, 
+    fontWeight:'bold'
   }
 });
 
